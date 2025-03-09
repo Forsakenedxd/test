509b32542cd2a4f44bd33fcc1364b61992eb76d9
RobloxGamerblx <sr.nikolay2004@gmail.com>
2025-03-09 16:39:31 +0500

    Update ForsakenScript.lua

--- ForsakenScript.lua
+++ ForsakenScript.lua
@@ -298,10 +298,3 @@ local Button = Tab:CreateButton({
       loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxGamerblx/test/refs/heads/main/Time%20Change"))()
    end,
 })
-
-local Button = Tab:CreateButton({
-      Name = "fullbright"
-      callback = function()
-         loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxGamerblx/test/refs/heads/main/timechange2"))()
-      end,
-})
