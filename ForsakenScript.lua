fb8065adf565a0c9d4d5f79ced01f863606881ed
RobloxGamerblx <sr.nikolay2004@gmail.com>
2025-03-28 21:59:13 +0500

    Update ForsakenScript.lua

--- ForsakenScript.lua
+++ ForsakenScript.lua
@@ -316,6 +316,7 @@ local Toggle = Tab:CreateToggle({
    Flag = "Enabled", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
 game:GetService("TextChatService").ChatWindowConfiguration.Enabled = Value
+wait(2)
    end,
 })
 
