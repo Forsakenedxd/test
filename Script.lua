a1ddfa0b7cdedbed7b1d8c47d6be9beb0932e7a8
Forsakenedxd <sr.nikolay2004@gmail.com>
2025-07-19 21:35:50 +0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxGamerblx/test/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Forsakenedxd/test/refs/heads/main/Loader.lua"))()
