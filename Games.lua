1f622a4c85e84e297cc0470d9a2d433ed17de7da
Forsakenedxd <sr.nikolay2004@gmail.com>
2025-07-19 21:35:21 +0500

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,5 @@
 local Games = {
- [18687417158] = "https://raw.githubusercontent.com/RobloxGamerblx/test/refs/heads/main/ForsakenScript.lua"
+ [18687417158] = "https://raw.githubusercontent.com/Forsakenedxd/test/refs/heads/main/ForsakenScript.lua"
 }
 
 return Games
