0bcd12f0c2195320c765b1ebd5bf27c91314b965
Forsakenedxd <sr.nikolay2004@gmail.com>
2025-07-19 16:29:55 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,3 +5,5 @@ for PlaceID, Execute in pairs(Games) do
         loadstring(game:HttpGet(Execute))()
     end
 end
+-- Thank to speedhub x to this code bc idk how i should do this
+-- THAT'S NOT MY LOADER!
